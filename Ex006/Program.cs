472f563d1ea647da049e2fa5c7b7c1cac6eb6172
Petru4e <Petru4e85@mail.ru>
2022-10-13 15:14:48 +0300

    Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿
+System.Console.WriteLine("Enter your name");
+string name =Console.ReadLine()!;
+if(name.ToLower()=="mary")
+{
+    System.Console.WriteLine($"Hello, {name}");
+}
+else
+{
+    System.Console.WriteLine($"Hi, {name}");
+}
