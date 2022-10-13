d29431c7f03685c5ac6696a278897e408038fc0c
Petru4e <Petru4e85@mail.ru>
2022-10-13 10:36:00 +0300

    Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = new Random().Next(1,10);
+System.Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+System.Console.WriteLine(numberB);
+System.Console.WriteLine(numberA+numberB);
