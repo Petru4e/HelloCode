d1f223eef5842210ca1089a8eabd2d548feace1c
Petru4e <Petru4e85@mail.ru>
2022-10-18 10:09:14 +0300

    Ex010

---
+++ Ex010/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] arr = {122,2,3,6,55,4,8,7,9};
+ int n = arr.Length;
+ int find = 6;
+ int index=0;
+ while (index<n)
+ {
+    if (arr[index]==find)
+    {
+        System.Console.WriteLine(index);
+    }
+    index++;
+ }
